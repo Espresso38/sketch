cfc62011b6629a83672d4a8818a65bfd6a7a6fd8
Espresso38 <naj.tomalak@gmail.com>
2024-07-10 14:06:43 +0200

    Creating divs for painting

--- script.js
+++ script.js
@@ -4,3 +4,15 @@
 3 dodać hover efect z losowym kolorem
 4 dodać reset płótna 
 */
+let divsContainer = document.getElementById("container");
+
+let dimension = 16;
+let totalDivs = dimension * dimension;
+let size = 540 / dimension;
+for (let i = 0; i < totalDivs; i++) {
+    let div = document.createElement("div");
+    div.style.background = "black";
+    div.style.width = size + "px";
+    div.style.height = size + "px";
+    divsContainer.appendChild(div);
+}
